0801b74de17b8bd964beab8344d60b6e8ca7489a
Erwan <erwan.legloannec@outlook.com>
2021-12-24 13:50:26 +0100

    fix: placed spawner entity type

--- src/main/kotlin/fr/pickaria/spawners/CollectSpawner.kt
+++ src/main/kotlin/fr/pickaria/spawners/CollectSpawner.kt
@@ -8,6 +8,7 @@ import org.bukkit.enchantments.Enchantment
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.BlockStateMeta
 
@@ -41,5 +42,14 @@ class CollectSpawner : Listener {
                 location.world!!.dropItemNaturally(location, itemStack)
             }
         }
+    }
+
+    @EventHandler
+    fun onBlockPlace(event: BlockPlaceEvent) {
+        if (event.block.type != Material.SPAWNER) return
+
+        // For some reasons, the spawned type isn't set when playing survival, this forces the spawn type to be correct
+        val spawner = (event.itemInHand.itemMeta as BlockStateMeta).blockState as CreatureSpawner
+        (event.blockPlaced.state as CreatureSpawner).spawnedType = spawner.spawnedType
     }
 }
