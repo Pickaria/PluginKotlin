c85e703fd65131738d9e8b5fd88562249b7a717c
Erwan <erwan.legloannec@outlook.com>
2022-01-07 10:36:10 +0100

    fix: chat ping

--- src/main/kotlin/fr/pickaria/tablist/ChatFormat.kt
+++ src/main/kotlin/fr/pickaria/tablist/ChatFormat.kt
@@ -1,7 +1,7 @@
 package fr.pickaria.tablist
 
-import fr.pickaria.Main
 import org.bukkit.Sound
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerChatEvent
@@ -10,25 +10,28 @@ import org.bukkit.event.player.AsyncPlayerChatEvent
 class ChatFormat: Listener {
 	@EventHandler
 	fun onAsyncPlayerChat(e: AsyncPlayerChatEvent) {
-		e.format = "§7%1\$s:§7 %2\$s"
-
-		val prefix = Main.chat.getPlayerPrefix(e.player).replace("&", "§")
-		e.player.setDisplayName(prefix + e.player.name)
+		e.format = "§7%1\$s » §7%2\$s"
 
 		// Player mention
+		val playerToRemove = mutableListOf<Player>()
+
 		for (player in e.recipients) {
-			if (e.message.contains(player.displayName)) {
-				e.recipients.remove(player)
+			if (e.message.lowercase().contains(player.name.lowercase())) {
+				playerToRemove.add(player)
 				player.playSound(player.location, Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1f, 1f)
 
 				player.sendMessage(
-					java.lang.String.format(
+					String.format(
 						e.format,
 						e.player.displayName,
-						e.message.replace(player.displayName, String.format("§6%s§r", player.displayName))
+						e.message.replace(player.name, String.format("§6%s§r", player.name))
 					)
 				)
 			}
+		}
+
+		for (player in playerToRemove) {
+			e.recipients.remove(player)
 		}
 	}
 }
